df4167f121e20fddb875ee020b601d0654eeed40
muhammadsodiq07 <muhammadsodiqkoziyev1@gmail.com>
2022-02-14 01:12:38 +0300

    added js

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let elopenmodal = document.getElementById('openModal');
+let elColsemodal = document.getElementById('close-btn');
+let elModal = document.getElementById('show');
+
+elopenmodal.addEventListener('click', function () {
+  elModal.classList.add('show');
+});
+
+elColsemodal.addEventListener('click', function () {
+  elModal.classList.remove('show');
+});
+
+var typed = new Typed('#element', {
+  // Waits 1000ms after typing "First"  
+  strings: [
+    'Web Developer', 'Developer'],
+  typedSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+
+var typed = new Typed('#developer', {
+  // Waits 1000ms after typing "First"  
+  strings: [
+    'Web Developer', 'Mukhammadsodiq'],
+  typedSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+let cursor = document.querySelector(".cursor");
+let cursorSpot = document.querySelector(".cursor-spot");
+
+document.addEventListener("mousemove", function(b){
+  cursor.style.cssText = cursorSpot.style.cssText = "left: " + b.clientX + "px; top: " + b.clientY + "px;";
+
+  const anchors =  document.querySelectorAll("a");
+  for(let a of anchors) {
+    a.onmouseover = () => {
+      cursor.classList.add("chover")
+    }
+    a.onmouseout = () => {
+      cursor.classList.remove("chover")
+    }
+  }
+});
