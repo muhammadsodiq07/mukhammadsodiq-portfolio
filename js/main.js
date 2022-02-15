8b3e22ff2dcc8e4f8bae3ac8a356fb2eb0c7aa70
muhammadsodiq07 <muhammadsodiqkoziyev1@gmail.com>
2022-02-15 08:59:18 +0300

    added

--- js/main.js
+++ js/main.js
@@ -54,7 +54,27 @@ document.addEventListener("mousemove", function(b){
 
 
 
+//Get the button
+var topbutton = document.getElementById("id-top");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    topbutton.style.opacity = "1";
+    topbutton.style.transition = "all 0.5s linner";
+  } else {
+    topbutton.style.opacity = "0";
+    topbutton.style.transition = "all 0.5s linner";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
 
